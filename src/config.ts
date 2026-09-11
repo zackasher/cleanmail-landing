23827f0614bff9d1b5d0034834259f3cce66190c
Zack Asher <calibrate.branding@gmail.com>
2026-09-11 09:36:29 -0500

    Launch: flip site stage to launched

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export type SiteStage = "waitlist" | "beta" | "launched";
-export const SITE_STAGE: SiteStage = "waitlist";
+export const SITE_STAGE: SiteStage = "launched";
 export const APP_STORE_URL = "https://apps.apple.com/app/id6777857105";
